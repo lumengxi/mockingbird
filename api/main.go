b940d3b89a473b6cd4116d2da85db310b9f4128a
Mengxi Lu <lumengxi@gmail.com>
2017-02-18 22:13:14 -0800

    switch to pat for mux

--- api/main.go
+++ api/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	log "github.com/Sirupsen/logrus"
-	"github.com/gorilla/mux"
+	"github.com/gorilla/pat"
 	_ "github.com/lib/pq"
 	"github.com/meatballhat/negroni-logrus"
 	"github.com/urfave/negroni"
 	"net/http"
-	"os"
 )
 
 
@@ -16,17 +15,17 @@ func init() {
 	InitDatabase("postgres://postgres:postgres@localhost/mockingbird?sslmode=disable")
 }
 
-// curl -X POST -d '{"name":"test", "config":{"body":"hello world!"}}' localhost:1234/mocker
 
 func main() {
 
-	router := mux.NewRouter()
+	router := pat.New()
 
 	// Routes
 	router.Handle("/", HandlerWithError{GetHomeHandler}).Methods("GET")
-	router.Handle("/mockers", HandlerWithError{GetMockerConfigsHandler}).Methods("GET")
-	router.Handle("/mocker", HandlerWithError{MakeMockerHandler}).Methods("POST")
-	router.Handle("/mockers?id={id}", HandlerWithError{GetMockerConfigHandler}).Methods("GET")
+	router.Handle("/create-mocker", HandlerWithError{MakeMockerHandler}).Methods("POST")
+	router.Handle("/show-mocker-configs", HandlerWithError{GetMockerConfigsHandler}).Methods("GET")
+	router.Handle("/show-mocker-config/{id}", HandlerWithError{GetMockerConfigHandler}).Methods("GET")
+	router.Handle("/mocker/{id}", HandlerWithError{GetMockerHandler}).Methods("GET")
 
 	n := negroni.New()
 	n.Use(negronilogrus.NewCustomMiddleware(log.DebugLevel, &log.JSONFormatter{}, "mockingbird"))
